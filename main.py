5bf0555bf4bcb2019524347e2b1892128b7b4791
unknown <dodo7se@gmail.com>
2024-11-10 13:38:43 +0900

    hello

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.get("/confirm")
+async def confirm():
+    return "hello"
+
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
